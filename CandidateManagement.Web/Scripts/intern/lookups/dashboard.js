ff88d386813d40e32c8e03f65a779494dc12329b
JHitchhiker <jai.holloway@hotmail.co.za>
2017-02-03 17:07:34 +0000

    Major dashboard overhaul

--- CandidateManagement.Web/Scripts/intern/lookups/dashboard.js
+++ CandidateManagement.Web/Scripts/intern/lookups/dashboard.js
@@ -8,6 +8,10 @@
     var labels = { visible: false };
     var successrate = 65.00;
     var daystojob = 1;
+    var signupRate = 0.00;
+    var contractStats = [];
+    var repeatRatio = 0.00;
+    var repeatValue = 0;
 
     var majorTicks = { size: '10%', interval: 10, visible: false },
         minorTicks = { size: '5%', interval: 2.5, style: { 'stroke-width': 1, stroke: '#aaaaaa' }, visible: false },
@@ -25,71 +29,23 @@
                     { name: 'EndingSoon', type: 'int' },
                     { name: 'DaysToJob', type: 'double' },
                     { name: 'SuccessRate', type: 'double' },
-                    { name: 'JobSeekers', type: 'int' }
+                    { name: 'JobSeekers', type: 'int' },
+                    { name: 'ConversionRate', type: 'double' },
+                    { name: 'YTDInterviews', type: 'double' },
+                    { name: 'RepeatContractors', type: 'double' },
+                    { name: 'RepeatRatio', type: 'double' },
+                    { name: 'OutOfContract', type: 'double' }
                 ],
                 url: '../Report/GetDashboard',
                 id: 'Id'
             };
 
-    var dataAdapter = new $.jqx.dataAdapter(source, {
-        loadComplete: function (records) {
-            // get data records.
-            var records = dataAdapter.records;
-            // get the length of the records array.
-            var length = records.length;
-            // loop through the records and display them in a table.
-            for (var i = 0; i < length; i++) {
-                var record = records[i];
-                averageinterviews = record.Average;
-                interviews = record.Interviews;
-                signups = record.SignUps;
-                ending = record.EndingSoon;
-                oncontract = record.OnContract;
-                //outofcontract = record.outofcontract;
-                successrate = record.SuccessRate;
-                daystojob = record.DaysToJob;
-
-
-                $('#interviewGauge').jqxGauge('value', interviews);
-                if (signups == 0)
-                {
-                    $('#signupGauge').jqxGauge('value', 0);
-                }
-                else
-                {
-                    $('#signupGauge').jqxGauge('value', signups / interviews * 100);
-                }
-                
-                $('#successRate').jqxGauge('value', successrate);
-
-                $("#interviews").text(interviews + ' Interviews');
-                $("#signups").text(signups + ' Sign Ups');
-                $("#oncontract").text(oncontract + ' On Contract');
-                $("#endingsoon").text(ending + ' Contracts Ending');
-                $("#outofcontract").text(outofcontract + ' Out of Contract');
-                $("#daystojob").text(daystojob + ' Days to Placement');
-                
-                //create an array variable and populate with the 3 stats and pass to knob...
-                $('#workingKnob').jqxBarGauge({
-                    
-                    values: [outofcontract, ending, oncontract], max: oncontract, tooltip: {
-                        visible: true, formatFunction: function (value) {
-                            var realVal = parseInt(value);
-                            return ('Year: 2016<br/>Price Index:' + realVal);
-                        },
-                    }
-                });
-            }
-        },
-        loadError: function (jqXHR, status, error) {
-        },
-        beforeLoadComplete: function (records) {
-        }
-    });
+    var dataAdapter;
     
-
+    
     var basicDemo = (function () {
         //Creating all page elements which are jqxWidgets
+        
         function _createElements() {
 
             $("#interviewNav, #statsNav").jqxNavBar({
@@ -111,7 +67,7 @@
                 height: 200
             });
             $('#interviewGauge').on('valueChanging', function (e) {
-                $('#interviewValue').text('Interview Performance');
+                $('#interviewValue').text(Math.round(e.args.value) + ' Interviews');
             });
             
             $('#signupGauge').jqxGauge({
@@ -132,18 +88,11 @@
                 $('#signupValue').text(Math.round(e.args.value) + '% Sign Up Rate');
             });
             
-            $('#workingKnob').jqxBarGauge({
-                colorScheme: "scheme02", height: 350,
-                values: [outofcontract, ending, oncontract], max: oncontract, tooltip: {
-                    visible: true, formatFunction: function (value) {
-                        var realVal = parseInt(value);
-                        return ('Year: 2016<br/>Price Index:' + realVal);
-                    },
-                }
-            });
-
             $('#successRate').jqxGauge({
                 labels: labels,
+                ranges: [{ startValue: 0, endValue: 25, style: { fill: '#e02629', stroke: '#e02629' }, endWidth: 5, startWidth: 5 },
+                         { startValue: 25, endValue: 50, style: { fill: '#fbd109', stroke: '#fbd109' }, endWidth: 5, startWidth: 5 },
+                         { startValue: 50, endValue: 100, style: { fill: '#4bb648', stroke: '#4bb648' }, endWidth: 5, startWidth: 5 }],
                 ticksMinor: minorTicks,
                 ticksMajor: majorTicks,
                 value: successrate,
@@ -157,6 +106,25 @@
                 $('#successValue').text(Math.round(e.args.value) + '% Successful Placings');
             });
             
+            $('#repeatRatio').jqxGauge({
+                ranges: [{ startValue: 0, endValue: 25, style: { fill: '#e02629', stroke: '#e02629' }, endWidth: 5, startWidth: 5 },
+                         { startValue: 25, endValue: 50, style: { fill: '#fbd109', stroke: '#fbd109' }, endWidth: 5, startWidth: 5 },
+                         { startValue: 50, endValue: 100, style: { fill: '#4bb648', stroke: '#4bb648' }, endWidth: 5, startWidth: 5 }],
+                ticksMinor: minorTicks,
+                ticksMajor: majorTicks,
+                value: repeatRatio,
+                min: 0,
+                labels: labels,
+                max: 100,
+                colorScheme: 'scheme05',
+                animationDuration: 1200,
+                height: 200
+            });
+            $('#repeatRatio').on('valueChanging', function (e) {
+                $('#repeatratioValue').text(Math.round(e.args.value) + '% Repeat Ratio');
+            });
+
+            $('#workingKnob').jqxBarGauge();
         };
 
         return {
@@ -165,29 +133,91 @@
             },
             init: function () {
                 dataAdapter.dataBind();
-                var records = dataAdapter.records;
-                // get the length of the records array.
-                var length = records.length;
-                // loop through the records and display them in a table.
-                //for (var i = 0; i < length; i++) {
-                //    var record = records[i];
-                //    averageinterviews = record.Average;
-                //    interviews = record.Interviews;
-                //    signups = record.SignUps;
-                //    ending = record.EndingSoon;
-                //    oncontract = record.OnContract;
-                //    outofcontract = record.outofcontract;
-                //    successrate = record.SuccessRate;
-                //    daystojob = record.DaysToJob;
-                //}
-                //Creating all jqxWindgets except the window
                 _createElements();
             }
         };
     }());
     $(document).ready(function () {
-        //Initializing the demo
-        dataAdapter.dataBind();
+        dataAdapter = new $.jqx.dataAdapter(source, {
+            loadComplete: function (records) {
+                var records = dataAdapter.records;
+                var length = records.length;
+
+                for (var i = 0; i < length; i++) {
+                    var record = records[i];
+                    averageinterviews = record.Average;
+                    interviews = record.Interviews;
+                    signups = record.SignUps;
+                    ending = record.EndingSoon;
+                    oncontract = record.OnContract;
+                    outofcontract = record.OutOfContract;
+                    successrate = record.SuccessRate;
+                    daystojob = record.DaysToJob;
+                    signupRate = record.ConversionRate;
+                    repeatRatio = record.RepeatRatio;
+                    repeatValue = record.RepeatContractors;
+
+                    $('#interviewGauge').jqxGauge('value', interviews);
+                    $('#signupGauge').jqxGauge('value', signupRate);
+                    $('#successRate').jqxGauge('value', successrate);
+                    $('#repeatRatio').jqxGauge('value', repeatRatio);
+
+                    $("#interviews").text(interviews + ' Interviews');
+                    $("#signups").text(signups + ' Sign Ups');
+                    $("#oncontract").text(oncontract + ' On Contract');
+                    $("#endingsoon").text(ending + ' Contracts Ending');
+                    $("#outofcontract").text(outofcontract + ' Out of Contract');
+                    $("#daystojob").text(daystojob + ' Days to Placement');
+                    $("#repeatcontractors").text(repeatValue + ' Repeat Contractors');
+
+                    $('#workingKnob').jqxBarGauge({
+                        colorScheme: 'rgb',
+                        customColorScheme: {
+                            name: 'rgb',
+                            colors: ['#e21818', '#fcf535', '#47db30']
+                        },
+                        height: 350,
+                        values: [outofcontract, ending, oncontract], max: oncontract,
+                        title: {
+                            text: 'Contractor Status',
+                            font: {
+                                size: 15
+                            },
+                            verticalAlignment: 'center',
+                            margin: 15
+                        },
+                        tooltip: {
+                            visible: true,
+                            formatFunction: function (value, index) {
+                                var stat;
+                                switch (index)
+                                {
+                                    case 2:
+                                        stat = "On Contract";
+                                        break;
+                                    case 1:
+                                        stat = "Ending Soon";
+                                        break;
+                                    case 0:
+                                        stat = "OUT OF CONTRACT";
+                                        break;
+                                }
+                                return (stat + ' ' + value);
+                            },
+                        }
+                    });
+                    $('#workingKnob').jqxBarGauge('render');
+                }
+            },
+            loadError: function (jqXHR, status, error) {
+            },
+            beforeLoadComplete: function (records) {
+            }
+        });
+
+        
+
         basicDemo.init();
+        
     });
 });
